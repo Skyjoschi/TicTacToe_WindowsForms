7367d5f6ff32c359e07f4186150fdc0c0d3ec1d5
Skyjoschi <100149120+Skyjoschi@users.noreply.github.com>
2023-08-08 14:32:32 +0200

    New version

--- SQLITE/MyTIcTacToe/tables.sql
+++ SQLITE/MyTIcTacToe/tables.sql
@@ -19,13 +19,20 @@ CREATE Table tblTime(
      FOREIGN KEY (id_user_name) REFERENCES tblUser (userName)
 )
 
+CREATE Table tblRanking(
+    status TEXT,
+    userWins INTEGER,
+     FOREIGN KEY (userWins) REFERENCES tblUser (userWins)
+)
+
+
+
 INSERT INTO 
     tblUser (userName, userPassword, userWins, userGamesCount)
 VALUES
     ("Josia", "2345", 3, 10),
     ("Samuel", "geheim", 5, 10),
     ("Jan", "Passwort!", 2, 10)
-
 
 
 INSERT INTO 
@@ -35,6 +42,22 @@ VALUES
     ("Samuel", 20210510, 50),
     ("Jan", 20230817, 3)
 
+
+INSERT INTO
+    tblRanking(status, userWins)
+Values
+    ("Diamond", 30),
+    ("Gold", 20),
+    ("Silver", 15),
+    ("Bronze", 10),
+    ("Clay", 5)
+
+
+-- Updaten
+UPDATE tblUser SET userWins = 2;
+UPDATE tblUser SET userWins = 10 WHERE userName = "Josia";
+
+
 -- Daten auswerten 
 SELECT 
  userID,
@@ -43,3 +66,11 @@ SELECT
  tblTime.timeDate as Datum
 FROM tblUser 
 LEFT JOIN tblTime on tblTime.id_user_name = tblUser.userName
+
+
+SELECT 
+ userID,
+ userName,
+ tblRanking.status
+FROM tblUser
+INNER JOIN tblRanking on tblUser.userWins = tblRanking.userWins
